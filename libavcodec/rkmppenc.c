ff036d693a5efce1a5e49b913254227a8bf23dac
le anh duc <ducla5@topica.edu.vn>
2017-09-15 01:31:59 +0700

    fix pix_fmt

--- libavcodec/rkmppenc.c
+++ libavcodec/rkmppenc.c
@@ -14,6 +14,8 @@
 #include "rockchip/mpp_common.h"
 #include "rockchip/mpp_log.h"
 
+//allocate mem --> mpi init --> mpp init --> configure some params --> encode
+
 const enum AVPixelFormat ff_rkmpp_pix_fmts[] = {
     AV_PIX_FMT_NV21,
     AV_PIX_FMT_YUV420P,
@@ -182,13 +184,229 @@ static MPP_RET mpi_enc_gen_osd_data(MppEncOSDData *osd_data, MppBuffer osd_buf, 
     return MPP_OK;
 }
 
+static int res_init(AVCodecContext *avctx){
+    int i;
+    MPP_RET ret = MPP_NOK;
+    MpiEncData *p = avctx->priv_data;
+    mpp_buffer_group_get_internal(&p->frm_grp, MPP_BUFFER_TYPE_ION);
+    mpp_buffer_group_get_internal(&p->pkt_grp, MPP_BUFFER_TYPE_ION);
+    for (i = 0; i < MPI_ENC_IO_COUNT; i++) {
+        //link frm_buff to frm_grp buffer
+        ret = mpp_buffer_get(p->frm_grp, &p->frm_buf[i], p->frame_size);
+        if (ret) {
+            return ret;
+        }
+
+        ret = mpp_buffer_get(p->frm_grp, &p->osd_idx_buf[i], p->osd_idx_size);
+        if (ret) {
+            return ret;
+        }
+
+        ret = mpp_buffer_get(p->pkt_grp, &p->pkt_buf[i], p->packet_size);
+        if (ret) {
+            return ret;
+        }
+
+        ret = mpp_buffer_get(p->pkt_grp, &p->md_buf[i], p->mdinfo_size);
+        if (ret) {
+            return ret;
+        }
+    }
+}
+static int mpi_init(AVCodecContext *avctx){
+    MPP_RET ret = MPP_NOK;
+    ret = mpp_create(&p->ctx, &p->mpi);
+    if (ret) {
+        return ret;
+    }
+
+    ret = mpp_init(p->ctx, MPP_CTX_ENC, p->type);
+    if (ret){
+        return ret;
+    }}
+static int mpp_init(AVCodecContext *avctx){
+    MpiEncData *p = avctx->priv_data;
+    MppApi *mpi;
+    MppCtx ctx;
+    MppEncCodecCfg *codec_cfg;
+    MppEncPrepCfg *prep_cfg;
+    MppEncRcCfg *rc_cfg;
+    MPP_RET ret = MPP_NOK;
+    mpi = p->mpi;
+    ctx = p->ctx;
+    codec_cfg = &p->codec_cfg;
+    prep_cfg = &p->prep_cfg;
+    rc_cfg = &p->rc_cfg;
+    p->fps = 30;
+    p->gop = 60;
+    p->bps = p->width * p->height / 8 * p->fps;
+    p->qp_init  = (p->type == MPP_VIDEO_CodingMJPEG) ? (10) : (26);
+
+    prep_cfg->change        = MPP_ENC_PREP_CFG_CHANGE_INPUT |
+                              MPP_ENC_PREP_CFG_CHANGE_ROTATION |
+                              MPP_ENC_PREP_CFG_CHANGE_FORMAT;
+    prep_cfg->width         = p->width;
+    prep_cfg->height        = p->height;
+    prep_cfg->hor_stride    = p->hor_stride;
+    prep_cfg->ver_stride    = p->ver_stride;
+    prep_cfg->format        = p->fmt;
+    prep_cfg->rotation      = MPP_ENC_ROT_0;
+    ret = mpi->control(ctx, MPP_ENC_SET_PREP_CFG, prep_cfg);
+    if (ret) {
+        return ret;
+    }
+    rc_cfg->change  = MPP_ENC_RC_CFG_CHANGE_ALL;
+    rc_cfg->rc_mode = MPP_ENC_RC_MODE_CBR;
+    rc_cfg->quality = MPP_ENC_RC_QUALITY_MEDIUM;
+    if (rc_cfg->rc_mode == MPP_ENC_RC_MODE_CBR) {
+        /* constant bitrate has very small bps range of 1/16 bps */
+        rc_cfg->bps_target   = p->bps;
+        rc_cfg->bps_max      = p->bps * 17 / 16;
+        rc_cfg->bps_min      = p->bps * 15 / 16;
+    } else if (rc_cfg->rc_mode ==  MPP_ENC_RC_MODE_VBR) {
+        if (rc_cfg->quality == MPP_ENC_RC_QUALITY_CQP) {
+            /* constant QP does not have bps */
+            rc_cfg->bps_target   = -1;
+            rc_cfg->bps_max      = -1;
+            rc_cfg->bps_min      = -1;
+        } else {
+            /* variable bitrate has large bps range */
+            rc_cfg->bps_target   = p->bps;
+            rc_cfg->bps_max      = p->bps * 17 / 16;
+            rc_cfg->bps_min      = p->bps * 1 / 16;
+        }
+    }
+
+    /* fix input / output frame rate */
+    rc_cfg->fps_in_flex      = 0;
+    rc_cfg->fps_in_num       = p->fps;
+    rc_cfg->fps_in_denorm    = 1;
+    rc_cfg->fps_out_flex     = 0;
+    rc_cfg->fps_out_num      = p->fps;
+    rc_cfg->fps_out_denorm   = 1;
+
+    rc_cfg->gop              = p->gop;
+    rc_cfg->skip_cnt         = 0;
+    ret = mpi->control(ctx, MPP_ENC_SET_RC_CFG, rc_cfg);
+    if (ret) {
+        return ret;
+    }
+    codec_cfg->coding = p->type;
+    switch (codec_cfg->coding) {
+    case MPP_VIDEO_CodingAVC : {
+        codec_cfg->h264.change = MPP_ENC_H264_CFG_CHANGE_PROFILE |
+                                 MPP_ENC_H264_CFG_CHANGE_ENTROPY |
+                                 MPP_ENC_H264_CFG_CHANGE_TRANS_8x8 |
+                                 MPP_ENC_H264_CFG_CHANGE_QP_LIMIT;
+        /*
+         * H.264 profile_idc parameter
+         * 66  - Baseline profile
+         * 77  - Main profile
+         * 100 - High profile
+         */
+        codec_cfg->h264.profile  = 100;
+        /*
+         * H.264 level_idc parameter
+         * 10 / 11 / 12 / 13    - qcif@15fps / cif@7.5fps / cif@15fps / cif@30fps
+         * 20 / 21 / 22         - cif@30fps / half-D1@@25fps / D1@12.5fps
+         * 30 / 31 / 32         - D1@25fps / 720p@30fps / 720p@60fps
+         * 40 / 41 / 42         - 1080p@30fps / 1080p@30fps / 1080p@60fps
+         * 50 / 51 / 52         - 4K@30fps
+         */
+        codec_cfg->h264.level    = 40;
+        codec_cfg->h264.entropy_coding_mode  = 1;
+        codec_cfg->h264.cabac_init_idc  = 0;
+        codec_cfg->h264.transform8x8_mode = 1;
+
+        if (rc_cfg->rc_mode == MPP_ENC_RC_MODE_CBR) {
+            /* constant bitrate do not limit qp range */
+            p->qp_max   = 48;
+            p->qp_min   = 4;
+            p->qp_step  = 16;
+            p->qp_init  = 0;
+        } else if (rc_cfg->rc_mode == MPP_ENC_RC_MODE_VBR) {
+            if (rc_cfg->quality == MPP_ENC_RC_QUALITY_CQP) {
+                /* constant QP mode qp is fixed */
+                p->qp_max   = p->qp_init;
+                p->qp_min   = p->qp_init;
+                p->qp_step  = 0;
+            } else {
+                /* variable bitrate has qp min limit */
+                p->qp_max   = 40;
+                p->qp_min   = 12;
+                p->qp_step  = 8;
+                p->qp_init  = 0;
+            }
+        }
+
+        codec_cfg->h264.qp_max      = p->qp_max;
+        codec_cfg->h264.qp_min      = p->qp_min;
+        codec_cfg->h264.qp_max_step = p->qp_step;
+        codec_cfg->h264.qp_init     = p->qp_init;
+    } break;
+    case MPP_VIDEO_CodingMJPEG : {
+        codec_cfg->jpeg.change  = MPP_ENC_JPEG_CFG_CHANGE_QP;
+        codec_cfg->jpeg.quant   = p->qp_init;
+    } break;
+    case MPP_VIDEO_CodingVP8 :
+    case MPP_VIDEO_CodingHEVC :
+    default : {
+        mpp_err_f("support encoder coding type %d\n", codec_cfg->coding);
+    } break;
+    }
+    ret = mpi->control(ctx, MPP_ENC_SET_CODEC_CFG, codec_cfg);
+    if (ret) {
+        return ret;
+    }
+
+    /* optional */
+    p->sei_mode = MPP_ENC_SEI_MODE_ONE_FRAME;
+    ret = mpi->control(ctx, MPP_ENC_SET_SEI_CFG, &p->sei_mode);
+    if (ret) {
+        return ret;
+    }
+
+    /* gen and cfg osd plt */
+    mpi_enc_gen_osd_plt(&p->osd_plt, p->plt_table);
+}
+
+/**
+ * ignore this, because FFMPEG helps us create file' headers
+ * @param avctx
+ * @return 
+ */
+static int prepare(AVCodecContext *avctx){
+    MPP_RET ret;
+    MppApi *mpi;
+    MppCtx ctx;
+    MpiEncData *p = avctx->priv_data;
+    MppPacket packet = NULL;
+    mpi = p->mpi;
+    ctx = p->ctx;
+    ret = mpi->control(ctx, MPP_ENC_GET_EXTRA_INFO, &packet);
+    if (ret) {
+        return ret;
+    }
+    if (packet) {
+        void *ptr   = mpp_packet_get_pos(packet); //get position of packet
+        size_t len  = mpp_packet_get_length(packet);//get valid data length of packet
+
+        if (p->fp_output)
+            //write data is pointed by ptr, has size is 1 byte, we have len item, to file
+            //we write global info to file
+            //see more: https://stackoverflow.com/questions/17541153/how-to-find-sps-and-pps-string-in-h264-codec-from-mp4
+            fwrite(ptr, 1, len, p->fp_output);
+
+        packet = NULL;
+    }
+}
+
 /**
  * Init something before starting
  * @param avctx
  * @return 
  */
 static av_cold int encode_init(AVCodecContext *avctx){
-    int i;
     MpiEncData *p = avctx->priv_data;//hold data for global using
     MPP_RET ret = MPP_NOK;
     p = mpp_calloc(MpiEncData, 1);
@@ -218,39 +436,9 @@ static av_cold int encode_init(AVCodecContext *avctx){
     p->plt_table[5] = MPP_ENC_OSD_PLT_RED;
     p->plt_table[6] = MPP_ENC_OSD_PLT_BLUE;
     p->plt_table[7] = MPP_ENC_OSD_PLT_BLACK;
-    mpp_buffer_group_get_internal(&p->frm_grp, MPP_BUFFER_TYPE_ION);
-    mpp_buffer_group_get_internal(&p->pkt_grp, MPP_BUFFER_TYPE_ION);
-    for (i = 0; i < MPI_ENC_IO_COUNT; i++) {
-        //link frm_buff to frm_grp buffer
-        ret = mpp_buffer_get(p->frm_grp, &p->frm_buf[i], p->frame_size);
-        if (ret) {
-            return ret;
-        }
-
-        ret = mpp_buffer_get(p->frm_grp, &p->osd_idx_buf[i], p->osd_idx_size);
-        if (ret) {
-            return ret;
-        }
-
-        ret = mpp_buffer_get(p->pkt_grp, &p->pkt_buf[i], p->packet_size);
-        if (ret) {
-            return ret;
-        }
-
-        ret = mpp_buffer_get(p->pkt_grp, &p->md_buf[i], p->mdinfo_size);
-        if (ret) {
-            return ret;
-        }
-    }
-    ret = mpp_create(&p->ctx, &p->mpi);
-    if (ret) {
-        return ret;
-    }
-
-    ret = mpp_init(p->ctx, MPP_CTX_ENC, p->type);
-    if (ret){
-        return ret;
-    }
+    res_init(avctx);    
+    mpi_init(avctx);
+    mpp_init(avctx);
     return 0;
 }
 
