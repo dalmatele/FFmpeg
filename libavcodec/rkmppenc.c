c6fead7901c8f311bf956434b60f280989078580
le anh duc <ducla5@topica.edu.vn>
2017-09-20 17:49:37 +0700

    Kiem tra thong so mpp

--- libavcodec/rkmppenc.c
+++ libavcodec/rkmppenc.c
@@ -574,13 +574,6 @@ static int encode_frame(AVCodecContext *avctx, AVPacket *pkt,
 //    http://www.ffmpeg-archive.org/How-to-get-raw-frame-data-from-AVFrame-data-and-AVFrame-linesize-without-specifying-the-pixel-format-td4661827.html
     size = av_image_copy_to_buffer(buf, mpp_buffer_get_size(frm_buf_in), 
             (const uint8_t **)frame->data, frame->linesize, frame->format,  frame->width, frame->height, 1);
-    for(i = 0; i < size; i++){
-        av_log(avctx, AV_LOG_ERROR, "%d ", *((uint8_t*)buf + i));
-        if((i % 8) == 0){
-            av_log(avctx, AV_LOG_ERROR, "\n");
-        }
-    }
-    av_log(avctx, AV_LOG_ERROR, "------------------\n");
     ///get data from frame
     //YUV = data[0] = y, data[1] = u, data[2] = v, linesize[i] la so byte can doc cho mot dong
 //    memcpy(buf, frame->data[0], frame->linesize[0]);
